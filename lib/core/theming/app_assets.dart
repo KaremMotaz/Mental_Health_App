592adb2584a707e9a55b9a7079929d7921b0e0aa
Karim Motaz <karemmotaz2929@gmail.com>
2026-01-06 16:12:47 +0200

    Add Assets Part 1

--- lib/core/theming/app_assets.dart
+++ lib/core/theming/app_assets.dart
@@ -1,11 +1,41 @@
 class AppAssets {
-  static const String start = 'assets/images/start.png';
   static const String emailIcon = 'assets/svgs/email.svg';
   static const String googleIcon = 'assets/svgs/google.svg';
   static const String appleIcon = 'assets/svgs/apple.svg';
   static const String facebookIcon = 'assets/svgs/facebook.svg';
   static const String authIcon = 'assets/svgs/auth_icon.svg';
   static const String passwordIcon = 'assets/svgs/password_icon.svg';
-  static const String welcomeBackground = 'assets/images/welcome.png';
   static const String arrowRight = 'assets/svgs/arrow_right.svg';
+  static const String arrowLeft = 'assets/svgs/arrow_left.svg';
+  static const String cardDot = 'assets/svgs/card_dot.svg';
+
+  static const String start = 'assets/images/start.png';
+  static const String welcomeBackground = 'assets/images/welcome.png';
+  static const String veryBad = 'assets/images/v_bad.png';
+  static const String bad = 'assets/images/bad.png';
+  static const String neutral = 'assets/images/neutral.png';
+  static const String good = 'assets/images/good.png';
+  static const String veryGood = 'assets/images/v_good.png';
+  static const String brain = 'assets/images/brain.png';
+  static const String cardBack = 'assets/images/card_back.png';
+  static const String cardEffect = 'assets/images/card_effect.png';
+  static const String community = 'assets/images/community.png';
+  static const String emotionalRegulation =
+      'assets/images/emotional_regulation.png';
+  static const String emotions = 'assets/images/emotions.png';
+  static const String glass = 'assets/images/glass.png';
+  static const String habits = 'assets/images/habits.png';
+  static const String meditationSessions =
+      'assets/images/meditation_sessions.png';
+  static const String moodTracking = 'assets/images/mood_tracking.png';
+  static const String motivation = 'assets/images/motivation.png';
+  static const String playCircle = 'assets/images/play_circle.png';
+  static const String positiveSun = 'assets/images/positive_sun.png';
+  static const String relationshipsConnections =
+      'assets/images/relationships_connections.png';
+  static const String relationships = 'assets/images/relationships.png';
+  static const String selfConfidence = 'assets/images/self_confidence.png';
+  static const String shadow = 'assets/images/shadow.png';
+  static const String sleep = 'assets/images/sleep.png';
+  static const String somethingElse = 'assets/images/something_else.png';
 }
